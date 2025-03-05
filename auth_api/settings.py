f3d18725862cca810b631b9916fc8960d61e1b50
edm-r <edmondyopa22@gmail.com>
2025-03-05 08:14:21 +0100

    Cors

--- auth_api/settings.py
+++ auth_api/settings.py
@@ -30,6 +30,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["rajapi-cop-auth-api.onrender.com"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://rajapi-cop-auth-api.onrender.com",
+]
 
 AUTH_USER_MODEL = 'auth_app.CustomUser'
 
@@ -51,7 +54,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
