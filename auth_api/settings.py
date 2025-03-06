23eb4f02a830a23585342e323124129064012164
edm-r <edmondyopa22@gmail.com>
2025-03-07 00:00:37 +0100

    settings upd1

--- auth_api/settings.py
+++ auth_api/settings.py
@@ -55,10 +55,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
